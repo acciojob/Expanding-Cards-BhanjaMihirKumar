564111ee77aab6582a8ebff3378be695c46d9739
Mihir kumar Bhanja <139785978+BhanjaMihirKumar@users.noreply.github.com>
2026-08-21 10:14:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+let panels = document.querySelectorAll(".panel");
+
+for(let i = 0; i < panels.length; i++){
+
+    panels[i].addEventListener("click", function(){
+
+        for(let j = 0; j < panels.length; j++){
+
+            panels[j].classList.remove("active");
+
+        }
+
+        panels[i].classList.add("active");
+
+    });
+
+}
